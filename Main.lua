9695a21520d075a06a1c29190eedbfaaa582dfd7
aerobatic prop manipulator <89122797+safariragoli2@users.noreply.github.com>
2022-06-18 13:53:35 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -18,13 +18,19 @@ local EnableButton = Instance.new("TextButton", MainFrame)
 local UIListLayout = Instance.new("UIListLayout", NotificationFrame)
 MainFrame.AnchorPoint = Vector2.new(0.5,0.05)
 MainFrame.Position = UDim2.fromScale(0.5,0.05)
-MainFrame.BackgroundColor3 = Color3.new(0,0,0)
-MainFrame.BackgroundTransparency = 0.85
+MainFrame.BackgroundTransparency = 1
 MainFrame.Size = UDim2.fromOffset(350,450)
 
 NotificationFrame.BackgroundColor3 = Color3.new(0,0,0)
 NotificationFrame.BackgroundTransparency = 0.85
 NotificationFrame.Size = UDim2.fromScale(1,1)
+
+EnableButton.AnchorPoint = Vector2.new(0,1)
+EnableButton.Position = UDim2.fromScale(0,1)
+EnableButton.Size = UDim2.new(1,0,0,50)
+EnableButton.TextSize = 25
+EnableButton.Font = Enum.Font.ArialBold
+EnableButton.Text = "Enable Auto-Report"
 
 function Report(plr, ctx, abusetype)
     Players:ReportAbuse(plr, abusetype, string.format("said \"%s\" which is inappropriate", ctx))
@@ -45,7 +51,7 @@ function NotifyAboutReport(plr, ctx, abusetype, parent)
 end
 
 function TrackPlayerChat(player)
-    if player == Players.LocalPlayer then return end
+    if player == Players.LocalPlayer or EnableAutoReport == false then return end
 
     player.Chatted:Connect(function(message)
         for word,abusetype in pairs(Dictionary) do
@@ -65,4 +71,9 @@ end
 
 Players.PlayerAdded:Connect(function(player)
     TrackPlayerChat(player)
+end)
+
+EnableButton.MouseButton1Click:Connect(function()
+    EnableAutoReport = not EnableAutoReport
+    EnableButton.Text = EnableAutoReport and "Disable Auto-Report" or "Enable Auto-Report"
 end)
