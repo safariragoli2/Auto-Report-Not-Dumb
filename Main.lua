d75451054905cb439f2aa8c72a2127b28f6c4072
aerobatic prop manipulator <89122797+safariragoli2@users.noreply.github.com>
2022-06-23 14:06:32 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -49,7 +49,7 @@ function NotifyAboutReport(plr, ctx, abusetype, word, parent)
     Notification.TextStrokeTransparency = 0
     Notification.Size = UDim2.new(1,0,0,65)
     Notification.Text = "Reported "..plr.." for saying the word '"..word.."' in the context of '"..ctx.."', because it counted as '"..abusetype.."'."
-    wait(5)
+    wait(8)
     Notification:Destroy()
 end
 
