c1cfac2c68c06788fdcdb6a603d132dd6bdc1b7b
aerobatic prop manipulator <89122797+safariragoli2@users.noreply.github.com>
2022-06-25 11:32:31 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -45,7 +45,7 @@ EnableButton.Text = "Enable Auto-Report"
 function Report(plr, ctx, abusetype)
     spawn(function()
         wait(Random.new():NextNumber(0.1,2))
-        Players:ReportAbuse(plr, abusetype, string.format("said \"%s\" which is inappropriate", ctx))
+        Players:ReportAbuse(plr, abusetype, string.format("said \"%s\" which is %s", ctx, string.lower(abusetype)))
     end)
 end
 
